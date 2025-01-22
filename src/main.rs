9fac74246b2ac64eaaa7f47162bb8359f556337f
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-21 18:45:35 -0600

    Assignment complete :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+fn apply_to_jobs(x: i32, title: &str) {
+    println!("I'm applying to {x} {title} jobs.")
+}
+
+fn is_even(x: i32) {
+    let even: bool = x % 2 == 0;
+    println!("{x} is {even}")
+}
+
+fn alphabets(text: &str) {
+    let x: bool = text.contains('a') || text.contains('A');
+    let y: bool = text.contains('z') || text.contains('Z');
+    
+    let container: (bool, bool) = (x, y);
+    print!("{container:#?}")
+
+
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    apply_to_jobs(32, "Rust Developer");
+
+    is_even(32);
+
+    alphabets("ZebrA");
 }
